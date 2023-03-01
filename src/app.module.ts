cb2a6dc3149618dd1b6ecb3a5298c044f69117b0
NaiGeLan <2129161589@qq.com>
2023-03-01 21:39:48 +0800

    redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
-
+import { ConfigModule } from '@nestjs/config';
+import { getConfig } from './utils';
 @Module({
-  imports: [UserModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ ignoreEnvFile: true, isGlobal: true, load: [] }),
+    UserModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,16 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { TransformInterceptor } from './common/interceptors/transform.interceptor';
+import { AllExceptionsFilter } from './common/exceptions/base.exception.filter';
+import { HttpExceptionFilter } from './common/exceptions/http.exception.filter';
+import { generateDocument } from './doc';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
-
+  // 接口版本化管理
   app.enableVersioning({
     defaultVersion: [VERSION_NEUTRAL, '1', '2'],
     type: VersioningType.URI,
@@ -21,6 +24,10 @@ async function bootstrap() {
   // 统一返回值格式
   app.useGlobalInterceptors(new TransformInterceptor());
 
-  await app.listen(9000);
+  // 异常过滤器
+  app.useGlobalFilters(new AllExceptionsFilter(), new HttpExceptionFilter());
+
+  generateDocument(app);
+  await app.listen(8888);
 }
 bootstrap();
